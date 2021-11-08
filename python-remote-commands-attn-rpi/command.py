ac38952aa360b711b93db289f5541e389a89abb0
Greg Wolff <gwolff@blues.com>
2021-11-08 10:38:49 -0500

    Adds module for managing app commands

---
+++ python-remote-commands-attn-rpi/command.py
@@ -0,0 +1,43 @@
+
+class CommandManager():
+    _Catalog = {}
+    def _submitTask(self, f, a):
+        pass
+
+    def IsCommand(self, id) -> bool:  
+        return id in self._Catalog
+
+    def SetSubmitTaskFn(self, f):
+        self._submitTask = f
+
+    def GetCommandFn(self, id):
+        return self._Catalog[id] if self.IsCommand(id) else None
+
+    def Enqueue(self, id, args):
+        if not self.IsCommand(id):
+            return
+        f = self.GetCommandFn(id)
+        self._submitTask(f, args)
+
+    def Add(self, id, fn):
+        self._Catalog[id] = fn
+
+
+
+def Print(content):
+    print(content)
+
+def Count(args={}):
+    x = args["min"] if "min" in args else 0
+    inc = args["inc"] if "inc" in args else 1
+    max = args["max"] if "max" in args else 10
+    while x <= max:
+        print(x)
+        x += inc
+
+
+Commands = CommandManager()
+
+
+Commands.Add("print", Print)
+Commands.Add("count", Count)
