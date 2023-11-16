f685d128454546bbc5af96f9897327560ea3559c
Greg Wolff <57912069+gwolffblues@users.noreply.github.com>
2023-11-16 16:47:55 -0500

    Perform file upload latency testing (#23)
    * add initial elements for general latency testing of the file or large data transfer
    
    * got test working on RPi

--- python-large-file-upload/main.py
+++ python-large-file-upload/main.py
@@ -1,13 +1,17 @@
 import serial
+#import wiringpi
 import notecard
 import logging
 import configargparse
 import time
 import binascii
 
+#wiringpi.wiringPiSetup()
 
 # Define default options
-DEFAULT_PORT_ID = "COM4"
+DEFAULT_PORT_TYPE = "serial"
+DEFAULT_SERIAL_PORT_ID = "COM4"
+DEFAULT_I2C_PORT_ID = "/dev/i2c-1"
 DEFAULT_PORT_BAUDRATE = 9600
 DEFAULT_DEBUG_TRANSACTIONS = True
 DEFAULT_WAIT_FOR_CONNECTION = True
@@ -33,24 +37,38 @@ def parseCommandLineArgs():
                                       default_config_files=['./config.txt'])
 
     p.add("-u","--product-uid", help="Notehub Product UID (com.company.name.project)", env_var="PRODUCT_UID", required=True)
-    p.add("-p", "--port", help="Serial port identifier for serial port connected to Notecard", default=DEFAULT_PORT_ID)
+    p.add("-p", "--port", help="Serial port identifier for port connected to Notecard")
     p.add("-b", "--baudrate", help="Serial port baudrate (bps)", default=DEFAULT_PORT_BAUDRATE, type = int)
     p.add("-r", "--route", help="Name of route Notecard web request transactions will use", default=DEFAULT_ROUTE_NAME)
     p.add("-d", "--debug-transactions", help="Display Notecard transactions", default=DEFAULT_DEBUG_TRANSACTIONS, type=lambda x:bool(str2bool(x)),nargs='?',const=True)
     p.add("-lf", "--log-folder", help="Directory where log files are stored", default=DEFAULT_LOG_FOLDER, env_var="LOG_FOLDER")
-    p.add("-f", "--file", help="File to use as data source for transfer", required=True)
+    p.add("-f", "--file", help="File to use as data source for transfer")
     p.add("-w", "--wait-for-connection", help="Wait until Notecard is connected to Notehub", default=DEFAULT_WAIT_FOR_CONNECTION, type=lambda x:bool(str2bool(x)),nargs='?',const=True)
     p.add("-m", "--mode", help="Notecard connection mode to Notehub (continuous, periodic, minimum)", default=DEFAULT_HUB_MODE)
     p.add("-s", "--chunk-size", help="Size of file chunk to transfer in bytes", default=DEFAULT_CHUNK_SIZE_BYTES, type=int)
     p.add("-t", "--timeout", help="Web request timeout in seconds", default=DEFAULT_WEB_REQUEST_TIMEOUT, type=int)
     p.add("-e", "--measure-elapsed-time", help="Measure how long the file transfer process takes", default=DEFAULT_MEASURE_TRANSFER_TIME, type=lambda x:bool(str2bool(x)),nargs='?',const=True )
+    p.add("-n", "--port-type", help="Select Serial or I2C port type", default=DEFAULT_PORT_TYPE)
+    p.add("-z", "--test-size", help="Size in Bytes of test data to use", default=0, type=int);
 
     opts = p.parse_args()
     return opts
 
 ## Get options
 opts = parseCommandLineArgs()
+
 use_temp_continuous = opts.mode != 'continuous'
+using_i2c = opts.port_type.lower() == "i2c"
+if opts.port is None:
+    opts.port = DEFAULT_I2C_PORT_ID if using_i2c else DEFAULT_SERIAL_PORT_ID
+
+if opts.file == None and opts.test_size == 0:
+    raise(Exception("Must provide either a file or a size of a test in bytes"))
+
+if opts.file != None and opts.test_size != 0:
+    raise(Exception("Cannot provide both a file and a size of a test in bytes"))
+
+using_file = opts.file != None
 
 ## Configure logging
 logFolder = opts.log_folder.rstrip("/\\")
@@ -67,8 +85,17 @@ logging.basicConfig(
 logging.info(opts)
 
 ## Connect to Notecard
-port = serial.Serial(opts.port, baudrate=opts.baudrate)
-card = notecard.OpenSerial(port, debug=opts.debug_transactions)
+port = None
+card = None
+if using_i2c:
+    # Use python-periphery on a Linux desktop or Raspberry Pi
+    from periphery import I2C
+    port = I2C(opts.port)
+    card = notecard.OpenI2C(port, 0, 0, debug=opts.debug_transactions)
+else: 
+    port = serial.Serial(opts.port, baudrate=opts.baudrate)
+    #port = wiringpi.serialOpen(opts.port,opts.baudrate)
+    card = notecard.OpenSerial(port, debug=opts.debug_transactions)
 
 ## Notecard Request Method
 def sendRequest(req, args=None, ignoreErrors = [], errRaisesException=True):
@@ -127,6 +154,20 @@ def writeWebReqChunk(payload, offset, total):
         raise Exception("Web Request Error: " + msg)
 
 
+def sendTestBytes(sizeOfTestData, chunk_size):
+    data = bytearray([7]*int(sizeOfTestData))
+
+    totalBytes = len(data)
+    numBytes = 0
+    while numBytes <= totalBytes:
+        e = min(numBytes + chunk_size, totalBytes)
+        try:
+            writeWebReqChunk(data[numBytes:e], numBytes, totalBytes)
+        except:
+            break
+
+        numBytes = e
+
 
 def sendFileBytes(filename):
     logging.info(f"Using file: {filename}")
@@ -183,12 +224,14 @@ if opts.wait_for_connection or use_temp_continuous:
     waitForConnection()
 
 startTime = 0
-if (opts.file):
+if using_file:
     startTime = time.time()
     sendFileBytes(opts.file)
     endTime = time.time()
 else:
-    logging.warning("No file selected to parse and send bytes")
+    startTime = time.time()
+    sendTestBytes(opts.test_size, opts.chunk_size)
+    endTime = time.time()
 
 
 if use_temp_continuous:
